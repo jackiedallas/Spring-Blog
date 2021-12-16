c917b206c1bc1fde884c16ff5418e9b7902360a7
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-16 15:07:56 -0600

    finished arithmetic operations

--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -8,10 +8,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MathController {
 
-    @GetMapping("/add/{number1}/and/{number2}")
+
+    public final String subtractMap = "/subtract/{number1}/and/{number2}";
+    public final String addMap = "/add/{number1}/and/{number2}";
+    public final String multiplyMap = "/multiply/{number1}/and/{number2}";
+    public final String divideMap = "/divide/{number1}/and/{number2}";
+
+    // add method
+    @GetMapping(addMap)
     @ResponseBody
     public String add(@PathVariable int number1, @PathVariable int number2) {
         return "<h1>" + number1 + " + " + number2 + " = " + (number1 + number2) + "</h1>";
+    }
+
+    // subtract method
+    @GetMapping(subtractMap)
+    @ResponseBody
+    public String subtract (@PathVariable int number1, @PathVariable int number2) {
+        return "<h1>" + number1 + " - " + number2 + " = " + (number1 - number2) + "</h1>";
+    }
+
+    // multiply method
+    @GetMapping(multiplyMap)
+    @ResponseBody
+    public String multiply (@PathVariable int number1, @PathVariable int number2) {
+        return "<h1>" + number1 + " x " + number2 + " = " + (number1 * number2) + "</h1>";
+    }
+
+    // divide method
+    @GetMapping(divideMap)
+    @ResponseBody
+    public String divide (@PathVariable int number1, @PathVariable int number2) {
+        return "<h1>" + number1 + " / " + number2 + " = " + (number1 / number2) + "</h1>";
     }
 
 }
