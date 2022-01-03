1ba99e43baab23eae0d75aeee7fa44358a9a26a9
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-03 15:12:24 -0600

    completed roll-dice exercise

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -1,15 +1,37 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RollDiceController {
 
+
+
+
     @GetMapping("/roll-dice")
-    @ResponseBody
     public String rollDice() {
         return "roll-dice";
     }
+
+    @GetMapping("/roll-dice/{number}")
+    @ResponseBody
+    public String guessNumber(@PathVariable int number, Model model) {
+        int randomNumber = (int) (Math.floor(Math.random() * 6) + 1);
+
+        model.addAttribute("number", number);
+        model.addAttribute("randomNumber", randomNumber);
+
+        if (number == randomNumber) {
+            return "<h1>Congrats! The correct number was " + randomNumber + "!</h1>";
+        }
+        return "<h1>You didn't guess the correct number. Number guessed was " + number + ". The correct number was " + randomNumber + ".</h1>";
+
+
+    }
+
+
+
+
 }
