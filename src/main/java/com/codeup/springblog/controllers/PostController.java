7a3dbc85735773ed0373873d587e3a6e1b6a33fd
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-03 23:22:28 -0600

    passed data to view

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,6 +2,7 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,14 +18,16 @@ public class PostController {
     // view posts
     @GetMapping("/posts")
     @ResponseBody
-    public String viewPosts() {
-        List<Post> posts = new ArrayList<>();
+    public String viewPosts(Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
 
-        Post post1 = new Post();
-        Post post2 = new Post();
+        Post post1 = new Post("Title 1", "Body 1");
+        Post post2 = new Post("Title 2", "Body 2");
 
         posts.add(post1);
         posts.add(post2);
+
+        model.addAttribute("posts", posts);
 
         return "/" + posts;
     }
@@ -32,8 +35,9 @@ public class PostController {
     // view individual post
     @GetMapping("/posts/{id}")
     @ResponseBody
-    public String viewSinglePost(@PathVariable int id) {
-        Post post = new Post();
+    public String viewSinglePost(@PathVariable int id, Model model) {
+        Post post = new Post("Title", "Body");
+        model.addAttribute("post", post);
         return post + "/" + id;
     }
 
