a632d629fa308e677a1b5bc98cb3a52746fa46c7
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-06 10:30:13 -0600

    added some js for dynamic date and added leaf image

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,7 @@
-// $('h1').css('color', 'red');
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+let date = new Date();
+let today = date.toLocaleDateString("en-US", options)
+
+// console.log(timeStamp);
+
+$('.date').append(today)
